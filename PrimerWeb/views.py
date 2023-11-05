b8a08ad6d2451041fbf50fd74475e507209862c9
Eduardo Vigna <ejvigna@gmail.com>
2023-11-05 20:54:01 -0300

    carga de la noche

--- PrimerWeb/views.py
+++ PrimerWeb/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from PrimerWeb import models
+from forms import *
 
-
-
-# def padre (request):
-#     return render(request, 'padre.html')
 
 def inicio (request):
     return render(request, 'PrimerWeb/inicio.html')
@@ -17,3 +14,43 @@ def Jugadores (request):
 
 def Deporte (request):
     return render(request, 'PrimerWeb/Deporte.html')
+
+def Carga_Deporte (request):
+    if request.method == 'POST':
+        deporte = CargaDeporte(request.post)
+        print(deporte)
+        if deporte.is_valid:
+            informacion = deporte.cleaned_data
+            deporte = Deporte(informacion["deporte"], informacion["jugadores"])
+            deporte.save()
+            return render(request, 'PrimerWeb/inicio.html')
+    else:
+        deporte = CargaDeporte()
+        return render(request, 'PrimerWeb/Carga_Deporte.html',{'deporte':deporte}) 
+    
+def Carga_Jugador (request):
+    if request.method == 'POST':
+        jugador = Cargajugador(request.post)
+        print(jugador)
+        if jugador.is_valid:
+            informacion = jugador.cleaned_data
+            jugador = Jugadores(informacion["nombre"], informacion["apellido"], informacion["edad"], informacion["diciplina"])
+            jugador.save()
+            return render(request, 'PrimerWeb/inicio.html')
+    else:
+        jugador = Cargajugador()
+        return render(request, 'PrimerWeb/Carga_Jugador.html',{'jugador':jugador})
+    
+def Carga_Club (request):
+    if request.method == 'POST':
+        club = CargaClub(request.post)
+        print(club)
+        if club.is_valid:
+            informacion = club.cleaned_data
+            club = Club(informacion["nombre"], informacion["ciudad"], informacion["fundacion"], informacion["socios"])
+            club.save()
+            return render(request, 'PrimerWeb/inicio.html')
+    else:
+        club = CargaClub()
+        return render(request, 'PrimerWeb/Carga_Club.html',{'club':club})
+            
