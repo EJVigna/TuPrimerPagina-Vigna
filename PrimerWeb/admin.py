009b2e5bbf7f3367090aa97887ebb94f8a91960a
Eduardo Vigna <ejvigna@gmail.com>
2023-11-05 15:03:25 -0300

    Agregamos informacion

--- PrimerWeb/admin.py
+++ PrimerWeb/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from PrimerWeb import models
+
 # Register your models here.
+
+admin.site.register(models.Club)
+admin.site.register(models.Deporte)
+admin.site.register(models.Jugador)
