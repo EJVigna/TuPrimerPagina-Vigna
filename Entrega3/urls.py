340f543a82b19fc6b053aa5646eef63607796687
Eduardo Vigna <ejvigna@gmail.com>
2023-11-06 22:05:58 -0300

    nueva carga lunes

--- Entrega3/urls.py
+++ Entrega3/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from PrimerWeb.views import *
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('PrimerWeb/', include('PrimerWeb.urls')),
 ]
