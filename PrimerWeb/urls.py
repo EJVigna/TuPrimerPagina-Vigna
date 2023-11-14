0517227ea829fc8094e5865bb96aeb2b3ba2eb38
Eduardo Vigna <ejvigna@gmail.com>
2023-11-13 21:34:36 -0300

    carga de lunes

--- PrimerWeb/urls.py
+++ PrimerWeb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from PrimerWeb import views
 
+
 app_name = 'PrimerWeb'
 
 urlpatterns = [
--- PrimerWeb/views.py
+++ PrimerWeb/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import Club, Jugador, Deporte
 from .forms import CargaDeporte, Cargajugador, CargaClub
 from django.http import HttpResponse
+from django.http.request import QueryDict
 
 def inicio (request):
     return render (request, 'inicio.html')
